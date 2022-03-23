70c0ead14a0c3f1ae09745937548a32afac18c3b
rksrbbn <rakasantang22@gmail.com>
2022-03-23 18:17:11 +0700

    add fitur edit password for user

--- server/src/modules/user/controllers/profileController.js
+++ server/src/modules/user/controllers/profileController.js
@@ -1,18 +1,5 @@
 const profile = require('../models/profile')
 
-// function formatter(data) {
-//     let	number_string = data.toString(),
-//     sisa 	= number_string.length % 3,
-//     rupiah 	= number_string.substr(0, sisa),
-//     ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
-        
-//     if (ribuan) {
-//         separator = sisa ? '.' : '';
-//         rupiah += separator + ribuan.join('.');
-//     }
-//     return rupiah;
-// }
-
 module.exports = {
     getProfile: (req,res) => {
         profile.getProfile(req.con,res,req.params.siswa_id, (err, rows) => {
@@ -49,11 +36,9 @@ module.exports = {
     },
 
     editProfile: (req,res) => {
-        profile.editProfile(req.con, res, req.files.img, req.params.siswa_id, (err, rows) => {
+        profile.editProfile(req.con, res, req.files.img, req.body.password, req.params.siswa_id, (err, rows) => {
             if (err) throw err
-            // console.log(req.files.img)
             res.json({erro: false, message: 'Data berhasil diubah'})
-            // res.json(rows)
         })
     }
         
