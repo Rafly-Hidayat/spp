e80d6307f31b6288d8e4fccc55249bb9fe7f84b2
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-03-24 20:38:35 +0700

    Upate

--- server/src/modules/user/models/p_bebas.js
+++ server/src/modules/user/models/p_bebas.js
@@ -49,11 +49,11 @@ module.exports = {
             (err, rows) => {
               if (err) throw err
               if (rows.length == 0) {
-                return res.json({error:true, message: "data tidak ditemukan."})
+                return res.json({ error: true, message: "data tidak ditemukan." })
               } else {
                 let bebas_id = rows[0].bebas_id
-                
-                con.query(`SELECT d_bebas_id, no_transaksi, d_bebas_bayar, d_bebas_deskripsi, d_bebas_tanggal, admin_nama, siswa_nama, siswa_nis, kelas_nama, jurusan_nama, d_kelas_nama FROM d_bebas JOIN bebas ON bebas.bebas_id = d_bebas.bebas_id JOIN siswa ON siswa.siswa_id = bebas.siswa_id JOIN kelas ON kelas.kelas_id = siswa.kelas_id JOIN jurusan ON jurusan.jurusan_id = siswa.siswa_id JOIN d_kelas ON d_kelas.d_kelas_id = siswa.d_kelas_id JOIN admin ON admin.admin_id = d_bebas.admin_id WHERE d_bebas.bebas_id = ${bebas_id}`, callback)
+
+                con.query(`SELECT d_bebas_id, no_transaksi, d_bebas_bayar, d_bebas_deskripsi, d_bebas_tanggal, admin_nama, siswa_nama, siswa_nis, kelas_nama, jurusan_nama, d_kelas_nama FROM d_bebas INNER JOIN bebas ON bebas.bebas_id = d_bebas.bebas_id INNER JOIN siswa ON siswa.siswa_id = bebas.siswa_id INNER JOIN kelas ON kelas.kelas_id = siswa.kelas_id INNER JOIN jurusan ON jurusan.jurusan_id = siswa.jurusan_id INNER JOIN d_kelas ON d_kelas.d_kelas_id = siswa.d_kelas_id INNER JOIN admin ON admin.admin_id = d_bebas.admin_id WHERE d_bebas.bebas_id = ${bebas_id}`, callback)
               }
             }
           );
