0188fcc671903cf785480f2854f921555e98f259
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-03-24 21:41:42 +0700

    Add invoice pembayaran bebas

--- server/src/modules/user/routes/p_bebasRouter.js
+++ server/src/modules/user/routes/p_bebasRouter.js
@@ -4,5 +4,6 @@ const p_bebasController = require('../controllers/p_bebasController')
 
 router.get('/user/pembayaran/bebas/:siswa_id', p_bebasController.getP_bebas)
 router.get('/user/detail/bebas/:siswa_id', p_bebasController.getDetail)
+router.get('/user/detail/bebas/:siswa_id/:d_bebas_id', p_bebasController.getInvoice)
 
 module.exports = router
