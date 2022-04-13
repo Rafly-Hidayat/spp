e82cde64e490cb0ba5195f85e47908201117897d
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-04-14 00:48:03 +0700

    update

--- server/src/modules/user/routes/p_bebasRouter.js
+++ server/src/modules/user/routes/p_bebasRouter.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const p_bebasController = require('../controllers/p_bebasController')
+const p_adminbebasController = require('../../admin/controllers/p_bebasController')
 
 router.get('/user/pembayaran/bebas/:siswa_id', p_bebasController.getP_bebas)
 router.get('/user/detail/bebas/:siswa_id', p_bebasController.getDetail)
 router.get('/user/detail/bebas/:siswa_id/:d_bebas_id', p_bebasController.getInvoice)
+router.get('/user/invoice/bebas/:d_bebas_id', p_adminbebasController.invoice)
 
 module.exports = router
--- server/src/modules/user/routes/p_bulananRouter.js
+++ server/src/modules/user/routes/p_bulananRouter.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const p_bulananController = require('../controllers/p_bulananController')
+const p_adminbulananController = require('../../admin/controllers/p_bulananController')
 
 router.get('/user/pembayaran/bulanan/:siswa_id', p_bulananController.getP_bulanan)
+router.get('/user/invoice/bulanan/:bulanan_id', p_adminbulananController.invoice)
 
 module.exports = router
